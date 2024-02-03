0bb073a7498a5f91b8c23f4c248bd39dd60a71f1
Ezzeri Esa <ezzeri.esa@gmail.com>
2024-02-02 17:58:05 -0800

    move to api directory

--- api/index.py
+++ api/index.py
@@ -7,6 +7,7 @@ from markupsafe import Markup
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     svg_image_url = None
@@ -16,21 +17,30 @@ def index():
     if request.method == "POST":
         url_text = escape(request.form["urlInput"])
 
+        import os
+        import sys
+        sys.stderr.write(f"{os.name}")
+
+        subprocess.run(['brew', 'install', 'git'])
+
         # Clone the GitHub repo (you may need to handle authentication here)
-        # subprocess.run(['git', 'clone', 'https://github.com/jxnl/instructor.git'])
+        subprocess.run(['git', 'clone', 'https://github.com/jxnl/instructor.git'])
 
         # Run pydeps to generate the SVG file
-        # subprocess.run(['pydeps', 'instructor/instructor'], check=True)
+        subprocess.run(['pydeps', 'instructor/instructor'], check=True)
 
         # Move the SVG file to the static directory and rename it (you can choose an appropriate name)
-        # subprocess.run(['mv', 'instructor.svg', 'static/instructor_deps.svg'], check=True)
+        subprocess.run(['mv', 'instructor.svg', 'public/instructor_deps.svg'], check=True)
 
         # Create the URL for the SVG image
-        svg_path = 'static/instructor_deps.svg'
+        svg_path = "public/instructor_deps.svg"
 
         if os.path.isfile(svg_path):
-            with open(svg_path, 'r') as svg_file:
+            with open(svg_path, "r") as svg_file:
                 svg_content = Markup(svg_file.read())
+
+        import sys
+        sys.stderr.write(f"here {type(svg_content)}")
 
     return render_template("index.html", svg_content=svg_content, url_text=url_text)
 
